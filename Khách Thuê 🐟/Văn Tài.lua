79ca472a53084812e8086d042f25124190f5cc44
NhatMinhVNQ <nhatminhvnz200@gmail.com>
2025-09-03 01:22:00 +0700

    Delete Khách Thuê 🐟/Văn Tài.lua

--- Khách Thuê 🐟/Văn Tài.lua
+++
@@ -1,3 +0,0 @@
-repeat wait() until game:IsLoaded() and game.Players.LocalPlayer 
-getgenv().Key = "47c26ad5cdaa8ce088976136" 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua"))()
